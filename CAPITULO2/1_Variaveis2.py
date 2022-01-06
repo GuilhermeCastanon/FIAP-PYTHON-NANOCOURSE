1a8e6ecc33fc9b82d7afa4740aa79cd3320f4a84
GuilhermeCastanon <rzzgame@hotmail.com>
2022-01-06 01:35:40 -0300

    Capitulo 2 Python - variaveis, tomada de decisao e lacos de repeticao

---
+++ CAPITULO2/1_Variaveis2.py
@@ -0,0 +1,6 @@
+destinatario = input("Digite o nome do destinatario: ")
+nome = input("Digite o nome da pessoa presente: ")
+evento = input("Digite o nome do evento: ")
+valor_gasto = int(input("Digite o valor gasto: "))
+
+print("Declaro para o senhor " + destinatario + " que o senhor " + nome + " esteve presente no evento " + evento + " e gastou o valor de R$ " + str(valor_gasto) + " com a entrada.")
