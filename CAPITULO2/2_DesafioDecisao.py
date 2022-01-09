654666b96901245e169395d2a07cb191de6c1bae
GuilhermeCastanon <rzzgame@hotmail.com>
2022-01-09 00:10:01 -0300

    Capitulo 2 Python - variaveis, tomada de decisao e lacos de repeticao

---
+++ CAPITULO2/2_DesafioDecisao.py
@@ -0,0 +1,12 @@
+nivel_acesso = input("Digite o nivel de acesso: ").upper()
+
+if(nivel_acesso == "ADM" or nivel_acesso == "USR"):
+    genero = input("Digite o seu genero: ").upper()
+    if(genero == "MASCULINO"):
+        print("Olá administrador")
+    else:
+        print("Olá administradora")
+elif(nivel_acesso == "GUEST"):
+    print("Olá visitante")
+else:
+    print("Olá desconhecido(a)")
---
+++ CAPITULO2/4_TabuadaFor.py
@@ -0,0 +1,4 @@
+numero = int(input("Digite um numero para sua tabuada ser exibida: "))
+
+for i in range(1,11):
+    print(str(numero) + " X " + str(i) +" = " +str(numero * i))
